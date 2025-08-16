941667e1f528e85f8abb47e752f9a97c2bea7e7c
Abem Woldesenbet <abemkibatu101@gmail.com>
2025-08-16 13:51:03 -0400

    Add car agent functionality and scene updates
    Updated `CarAgent.cs` to implement episode management, observation collection, and action handling for a car agent in Unity ML-Agents. Added namespaces for sensors and actuators, and initialized a `ControlInterface`.
    
    Modified `SampleScene.unity` to include new GameObjects such as `CameraSensor` and `Camera`, along with a `MeshCollider`. Adjusted prefab instances to integrate these components into the scene.
    
    Updated `MonoBehaviour` settings to include properties for car control (Gas, Brake, Left, Right, HandBrake), initializing them to zero for proper action handling.

--- Assets/CarAgent.cs
+++ Assets/CarAgent.cs
@@ -2,18 +2,48 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using Unity.MLAgents;
+using Unity.MLAgents.Sensors;
+using Unity.MLAgents.Actuators;
 
 public class CarAgent : Agent
 {
+    private ControlInterface controlInterface;
     // Start is called before the first frame update
     void Start()
     {
-        
+        controlInterface = GetComponent<ControlInterface>();
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+    public override void OnEpisodeBegin()
+    {
+        Vector3 startPosition = new Vector3(Random.Range(-50f, 57f), 0.51f, 42);
+        transform.position = startPosition;
+
+        Vector3 startRotation = new Vector3(0, Random.Range(0f, 360f), 0);
+        transform.rotation = Quaternion.Euler(startRotation);
+    }
+    public override void CollectObservations(VectorSensor sensor)
+    {
+        sensor.AddObservation(transform.position);
+        sensor.AddObservation(transform.rotation.eulerAngles);
+        // velocity
+        sensor.AddObservation(GetComponent<Rigidbody>().velocity);
+        sensor.AddObservation(transform.forward);
+    }
+    public override void OnActionReceived(ActionBuffers actions)
+    {
+        var actionsDiscrete = actions.DiscreteActions;
+
+        controlInterface.Gas = actionsDiscrete[0] == 1;
+        controlInterface.Brake = actionsDiscrete[0] == 1;
+        controlInterface.Left = actionsDiscrete[1] == 1;    
+        controlInterface.Right = actionsDiscrete[1] == 1;
+        controlInterface.HandBrake = actionsDiscrete[2] == 1;
+
     }
 }
